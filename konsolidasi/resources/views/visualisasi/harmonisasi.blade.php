726646f39377e0ebeb7927eeca60e25d77bbafbc
xthaj <athajuli@gmail.com>
2025-02-18 23:06:37 +0700

    moarr fe charts stuff. got the scroll to work. json!

--- konsolidasi/resources/views/visualisasi/harmonisasi.blade.php
+++ konsolidasi/resources/views/visualisasi/harmonisasi.blade.php
@@ -84,23 +84,9 @@
     </x-slot>
 
     <div id="visualizationCanvas" class="w-full p-4 md:overflow-y-auto md:h-full transition-all duration-300 dark:bg-gray-900" :class="{ 'md:w-full': !isBuilderVisible }">
-        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div class="bg-white  md:h-auto p-4 rounded-lg shadow-md relative dark:bg-gray-800 col-span-1 lg:col-span-2 xl:col-span-3">
                 <h3 class="text-lg font-bold">Inflasi</h3>
-
-                <!-- <div x-data="{ selectedDatasets: [] }" class="flex flex-wrap gap-2 mb-4">
-                    <template x-for="(dataset, index) in datasets" :key="index">
-                        <label class="flex items-center space-x-2">
-                            <input type="checkbox"
-                                :id="'checkbox-' + index"
-                                x-model="selectedDatasets"
-                                :value="dataset.label"
-                                class="form-checkbox h-4 w-4 text-blue-600">
-                            <span x-text="dataset.label"></span>
-                        </label>
-                    </template>
-                </div> -->
-
                 <div class="flex justify-end space-x-2 mb-2">
                     <button onclick="showInflasiLine()" class="bg-blue-500 text-white px-4 py-2 rounded">Show Inflasi</button>
                     <button onclick="showAndilLine()" class="bg-green-500 text-white px-4 py-2 rounded">Show Andil</button>
@@ -108,26 +94,43 @@
                 </div>
                 <canvas id="multiAxisChart" class="w-full max-h-96 md:h-auto"></canvas>
             </div>
+
             <div class="bg-white md:h-auto p-4 rounded-lg shadow-md relative dark:bg-gray-800 col-span-1 lg:col-span-2 xl:col-span-3">
                 <h3 class="text-lg font-bold">Inflasi & Andil Maret 2025</h3>
-                <div class="flex justify-end space-x-2 mb-2">
-                    <button onclick="showInflasi()" class="bg-blue-500 text-white px-4 py-2 rounded">Show Inflasi</button>
-                    <button onclick="showAndil()" class="bg-green-500 text-white px-4 py-2 rounded">Show Andil</button>
-                    <button onclick="showBoth()" class="bg-gray-500 text-white px-4 py-2 rounded">Show Both</button>
-                </div>
                 <canvas id="horizontalBarChart" class="w-full max-h-96 md:h-auto"></canvas>
             </div>
-            <div class="bg-white p-4 rounded-lg shadow-md relative dark:bg-gray-800">
-                <h3 class="text-lg font-bold">Growth Rate</h3>
-                <canvas id="lineChart" class="w-full h-64 md:h-auto"></canvas>
+
+            <div class="bg-white md:h-auto p-4 rounded-lg shadow-md relative dark:bg-gray-800 col-span-1 lg:col-span-2 xl:col-span-3">
+                <h3 class="text-lg font-bold">Jumlah Provinsi berdasarkan Arah Inflasi</h3>
+                <canvas id="stackedBarChart" class="w-full max-h-96 md:h-auto"></canvas>
             </div>
-            <div class="bg-white p-4 rounded-lg shadow-md relative dark:bg-gray-800">
-                <h3 class="text-lg font-bold">Category Distribution</h3>
-                <canvas id="pieChart" class="w-full h-64 md:h-auto"></canvas>
+
+            <!-- <div class="bg-white md:h-auto p-4 rounded-lg shadow-md relative dark:bg-gray-800 col-span-1 lg:col-span-2 xl:col-span-3">
+                <h3 class="text-lg font-bold">Peringkat inflasi</h3>
+                <div class="overflow-y-auto max-h-96">
+                    <canvas id="rankBarChart" class="w-full"></canvas>
+                </div>
+            </div> -->
+
+            <div class="bg-white md:h-auto p-4 rounded-lg shadow-md relative dark:bg-gray-800 ">
+                <h3 class="text-lg font-bold">Inflasi Provinsi</h3>
+                <!-- <div class="overflow-x-auto"> -->
+                <div class="max-h-64 overflow-y-auto">
+                    <div class="h-[1000px]"> <!-- Set a large height to make it scrollable -->
+                        <canvas id="rankBarChartProvinsi" class="w-full h-full"></canvas>
+                    </div>
+                </div>
+                <!-- </div> -->
             </div>
-            <div class="bg-white p-4 rounded-lg shadow-md relative dark:bg-gray-800">
-                <h3 class="text-lg font-bold">Area Chart</h3>
-                <canvas id="areaChart" class="w-full h-64 md:h-auto"></canvas>
+            <div class="bg-white md:h-auto p-4 rounded-lg shadow-md relative dark:bg-gray-800">
+                <h3 class="text-lg font-bold">Inflasi Kabupaten/Kota</h3>
+                <!-- <div class="overflow-x-auto"> -->
+                    <canvas id="rankBarChartKabkot" class="w-full max-h-96 md:h-auto"></canvas>
+                <!-- </div> -->
+            </div>
+
+            <div class="bg-white md:h-auto p-4 rounded-lg shadow-md relative dark:bg-gray-800 col-span-1 lg:col-span-2 xl:col-span-3">
+                <h3 class="text-lg font-bold">Map</h3>
             </div>
         </div>
     </div>
@@ -210,50 +213,144 @@
         });
 
         const labels = ['November 2024', 'December 2024', 'January 2025', 'February 2025', 'March 2025'];
+
         const datasets = [
             {
                 label: 'Harga Produsen',
-                data: [5.5, 3.0, 2.8, 3.2, -3.1], // Inflasi values
+                inflasi: [5.5, 3.0, 2.8, 3.2, -3.1], // Inflasi values
                 andil: [0.5, 0.6, 0.55, 0.65, 0.6], // Corresponding andil values
-
+                stacked:[20,30,50]
             },
             {
                 label: 'Harga Produsen Desa',
-                data: [3.8, 2.1, 2.0, 2.3, 2.2],
+                inflasi: [3.8, 2.1, 2.0, 2.3, 2.2],
                 andil: [0.4, 0.45, 0.43, 0.47, 0.44],
+                stacked:[20,30,50]
 
             },
             {
                 label: 'Harga Perdagangan Besar',
-                data: [2.5, -3.0, 2.8, -3.2, -3.1], // Inflasi values
+                inflasi: [2.5, -3.0, 2.8, -3.2, -3.1], // Inflasi values
                 andil: [0.5, 0.80, 0.34, 0.15, 0.6], // Corresponding andil values
+                stacked:[20,30,50]
 
             },
             {
                 label: 'Harga Konsumen Desa',
-                data: [-1.8, 3.1, 4.0, 2.9, 7.2],
+                inflasi: [-1.8, 3.1, 4.0, 2.9, 7.2],
                 andil: [0.4, 0.30, 0.68, 0.25, 0.43],
+                stacked:[20,30,50]
 
             },
             {
                 label: 'Harga Konsumen Kota',
-                data: [2.5, 3.0, 7.0, 3.2, 3.1], // Inflasi values
+                inflasi: [2.5, 3.0, 7.0, 3.2, 3.1], // Inflasi values
                 andil: [0.22, 0.52, 0.32, 0.65, 0.6], // Corresponding andil values
+                stacked:[20,30,50]
 
             },
             // Add more datasets for other price levels as needed
         ];
 
+        const dataset3 = [
+
+        ];
+
+        const provinsi = [
+            "ACEH", "SUMATERA UTARA", "SUMATERA BARAT", "RIAU", "JAMBI", "SUMATERA SELATAN", "BENGKULU",
+            "LAMPUNG", "KEPULAUAN BANGKA BELITUNG", "KEPULAUAN RIAU", "DKI JAKARTA", "JAWA BARAT",
+            "JAWA TENGAH", "DI YOGYAKARTA", "JAWA TIMUR", "BANTEN", "BALI", "NUSA TENGGARA BARAT",
+            "NUSA TENGGARA TIMUR", "KALIMANTAN BARAT", "KALIMANTAN TENGAH", "KALIMANTAN SELATAN",
+            "KALIMANTAN TIMUR", "KALIMANTAN UTARA", "SULAWESI UTARA", "SULAWESI TENGAH", "SULAWESI SELATAN",
+            "SULAWESI TENGGARA", "GORONTALO", "SULAWESI BARAT", "MALUKU", "MALUKU UTARA", "PAPUA BARAT",
+            "PAPUA BARAT DAYA", "PAPUA", "PAPUA SELATAN", "PAPUA TENGAH", "PAPUA PEGUNUNGAN"
+        ];
+
+        const kota = [
+            "KAB JAYAWIJAYA", "KAB NABIRE", "TIMIKA", "MERAUKE", "KOTA JAYAPURA", "KOTA SORONG",
+            "KAB SORONG SELATAN", "KAB SORONG", "MANOKWARI", "KOTA TERNATE", "KAB HALMAHERA TENGAH",
+            "KOTA TUAL", "KOTA AMBON", "KAB MALUKU TENGAH", "MAMUJU", "KAB MAJENE", "KOTA GORONTALO",
+            "KAB GORONTALO", "KOTA BAU BAU", "KOTA KENDARI", "KAB KOLAKA", "KAB KONAWE", "KOTA PALOPO",
+            "KOTA PARE PARE", "KOTA MAKASSAR", "KAB LUWU TIMUR", "KAB SIDENRENG RAPPANG", "KAB WAJO",
+            "WATAMPONE", "BULUKUMBA", "KOTA PALU", "KAB TOLI TOLI", "KAB MOROWALI", "LUWUK",
+            "KOTA KOTAMOBAGU", "KOTA MANADO", "KAB MINAHASA UTARA", "KAB MINAHASA SELATAN", "KOTA TARAKAN",
+            "KAB NUNUKAN", "TANJUNG SELOR", "KOTA SAMARINDA", "KOTA BALIKPAPAN", "KAB PENAJAM PASER UTARA",
+            "KAB BERAU", "KOTA BANJARMASIN", "TANJUNG", "KAB HULU SUNGAI TENGAH", "KOTABARU",
+            "KAB TANAH LAUT", "KOTA PALANGKARAYA", "KAB SUKAMARA", "KAB KAPUAS", "SAMPIT",
+            "KOTA SINGKAWANG", "KOTA PONTIANAK", "KAB KAYONG UTARA", "SINTANG", "KAB KETAPANG",
+            "KOTA KUPANG", "KAB NGADA", "MAUMERE", "KAB TIMOR TENGAH SELATAN", "WAINGAPU",
+            "KOTA BIMA", "KOTA MATARAM", "KAB SUMBAWA", "KOTA DENPASAR", "SINGARAJA", "KAB BADUNG",
+            "KAB TABANAN", "KOTA SERANG", "KOTA CILEGON", "KOTA TANGERANG", "KAB LEBAK", "KAB PANDEGLANG",
+            "KOTA SURABAYA", "KOTA MADIUN", "KOTA PROBOLINGGO", "KOTA MALANG", "KOTA KEDIRI", "SUMENEP",
+            "KAB GRESIK", "KAB BOJONEGORO", "BANYUWANGI", "JEMBER", "KAB TULUNGAGUNG", "KOTA YOGYAKARTA",
+            "KAB GUNUNGKIDUL", "KOTA TEGAL", "KOTA SEMARANG", "KOTA SURAKARTA", "KUDUS", "KAB REMBANG",
+            "KAB WONOGIRI", "KAB WONOSOBO", "PURWOKERTO", "CILACAP", "KOTA TASIKMALAYA", "KOTA DEPOK",
+            "KOTA BEKASI", "KOTA CIREBON", "KOTA BANDUNG", "KOTA SUKABUMI", "KOTA BOGOR", "KAB SUBANG",
+            "KAB MAJALENGKA", "KAB BANDUNG", "DKI JAKARTA", "KOTA TANJUNG PINANG", "KOTA BATAM",
+            "KAB KARIMUN", "KOTA PANGKAL PINANG", "KAB BELITUNG TIMUR", "KAB BANGKA BARAT", "TANJUNG PANDAN",
+            "KOTA METRO", "KOTA BANDAR LAMPUNG", "KAB MESUJI", "KAB LAMPUNG TIMUR", "KOTA BENGKULU",
+            "KAB MUKO MUKO", "KOTA LUBUK LINGGAU", "KOTA PALEMBANG", "KAB MUARA ENIM", "KAB OGAN KOMERING ILIR",
+            "KOTA JAMBI", "MUARA BUNGO", "KAB KERINCI", "KOTA DUMAI", "KOTA PEKANBARU", "KAB KAMPAR",
+            "TEMBILAHAN", "KOTA BUKITTINGGI", "KOTA PADANG", "KAB PASAMAN BARAT", "KAB DHARMASRAYA",
+            "KOTA GUNUNGSITOLI", "KOTA PADANGSIDIMPUAN", "KOTA MEDAN", "KOTA PEMATANG SIANTAR", "KOTA SIBOLGA",
+            "KAB DELI SERDANG", "KAB KARO", "KAB LABUHANBATU", "KOTA LHOKSEUMAWE", "KOTA BANDA ACEH",
+            "KAB ACEH TAMIANG", "MEULABOH", "KAB ACEH TENGAH"
+        ];
+
+        const inflasi_provinsi = [
+            2.5, 3.1, 2.8, 3.3, 2.9, 3.5, 2.7, -3.0, 3.2, 2.6,
+            2.9, 3.4, 3.1, 2.7, 3.6, 2.8, 3.3, 3.0, 2.5, 3.2,
+            3.1, 2.9, 3.5, -2.8, 3.0, 2.7, 3.3, 3.1, 2.6, 3.4,
+            2.9, 3.0, 2.5, 2.8, 3.2, 3.1, -2.7, 3.5
+        ];
+
+        const chartData = {
+            labels: [
+                "Harga Produsen",
+                "Harga Produsen Desa",
+                "Harga Perdagangan Besar",
+                "Harga Konsumen Desa",
+                "Harga Konsumen Kota"
+            ],
+            datasets: [
+                {
+                    label: "Menurun (↓)",
+                    data: [45, 30, 25, 50, 40], // Values for each harga level
+                    backgroundColor: "#ff6384", // Red
+                    stack: "stack1"
+                },
+                {
+                    label: "Naik (↑)",
+                    data: [20, 40, 35, 20, 30], // Values for each harga level
+                    backgroundColor: "#36a2eb", // Blue
+                    stack: "stack1"
+                },
+                {
+                    label: "Stabil (-)",
+                    data: [35, 30, 40, 30, 30], // Values for each harga level
+                    backgroundColor: "#4bc0c0", // Green
+                    stack: "stack1"
+                }
+            ]
+        };
+
+
                 // Get the context of the canvas element
         var ctx = document.getElementById('multiAxisChart').getContext('2d');
         var hbx = document.getElementById('horizontalBarChart').getContext('2d');
+        var sbx = document.getElementById('stackedBarChart').getContext('2d');
 
         // Create the chart
         var multiAxisChart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: labels,
-                datasets: datasets
+                datasets: datasets.map(dataset => ({
+                    label: dataset.label,
+                    data: dataset.inflasi,
+                    inflasi: dataset.inflasi,
+                    andil: dataset.andil,
+                }))
             },
             options: {
                 responsive: true,
@@ -273,7 +370,7 @@
                         callbacks: {
                             label: function(context) {
                                 const dataset = context.dataset;
-                                const inflasi = context.raw;
+                                const inflasi = dataset.inflasi[context.dataIndex];
                                 const andil = dataset.andil[context.dataIndex];
                                 return [
                                     `${dataset.label}`,
@@ -303,6 +400,7 @@
         });
 
         var latestMonthIndex = labels.length - 1;
+
         var horizontalBarChart = new Chart(hbx, {
             type: 'bar',
             options: {
@@ -317,59 +415,27 @@
             },
             data: {
                 labels: datasets.map(dataset => dataset.label),
-                datasets: [{
-                    label: 'Inflasi',
-                    data: datasets.map(dataset => dataset.data[latestMonthIndex]),
-
-                }]
+                datasets: [
+                    {
+                        label: 'Inflasi',
+                        data: datasets.map(dataset => dataset.inflasi[latestMonthIndex]), // Data inflasi
+                    },
+                    {
+                        label: 'Andil',
+                        data: datasets.map(dataset => dataset.andil[latestMonthIndex]), // Data andil
+                    },
+                ],
             },
         });
 
-
-        function showInflasi() {
-
-            horizontalBarChart.data.datasets = [{
-                label: 'Inflasi',
-                data: datasets.map(dataset => dataset.data[latestMonthIndex]),
-
-            }];
-            horizontalBarChart.update();
-        }
-
-        function showAndil() {
-            horizontalBarChart.data.datasets = [{
-                label: 'Andil',
-                data: datasets.map(dataset => dataset.andil[latestMonthIndex]),
-
-            }];
-            horizontalBarChart.update();
-        }
-
-        function showBoth() {
-            horizontalBarChart.data.datasets = [
-                {
-                    label: 'Inflasi',
-                    data: datasets.map(dataset => dataset.data[latestMonthIndex]),
-
-                },
-                {
-                    label: 'Andil',
-                    data: datasets.map(dataset => dataset.andil[latestMonthIndex]),
-
-                }
-            ];
-            horizontalBarChart.update();
-        }
-
-
         function showInflasiLine() {
-            // Assuming selectedDatasets is available in your Alpine component
-            const filteredDatasets = datasets.filter(dataset => selectedDatasets.includes(dataset.label));
 
             multiAxisChart.data.datasets = datasets.map(dataset => ({
                 label: dataset.label,
-                data: dataset.data,
-            }));
+                data: dataset.inflasi,
+                inflasi: dataset.inflasi,
+                andil: dataset.andil,
+            }))
             multiAxisChart.update();
         }
 
@@ -377,6 +443,8 @@
             multiAxisChart.data.datasets = datasets.map(dataset => ({
                 label: dataset.label,
                 data: dataset.andil,
+                inflasi: dataset.inflasi,
+                andil: dataset.andil,
             }));
             multiAxisChart.update();
         }
@@ -385,11 +453,17 @@
             multiAxisChart.data.datasets = datasets.flatMap(dataset => [
                 {
                     label: `${dataset.label} - Inflasi`,
-                    data: dataset.data,
+                    data: dataset.inflasi,
+                    inflasi: dataset.inflasi,
+                    andil: dataset.andil,
+                    borderColor: "#4FCFCF",
                 },
                 {
                     label: `${dataset.label} - Andil`,
                     data: dataset.andil,
+                    inflasi: dataset.inflasi,
+                    andil: dataset.andil,
+                    borderColor: 'gray',
                 }
             ]);
             multiAxisChart.update();
@@ -410,6 +484,73 @@
             }
         }
 
+        var stackedBarChart = new Chart(sbx, {
+            type: "bar",
+            data: chartData,
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'bottom',
+                    },
+                },
+                scales: {
+                    x: { stacked: true },
+                    y: { stacked: true, beginAtZero: true, max: 100 }
+                }
+            }
+        });
+
+        const provinsiInflasi = provinsi.map(() => (Math.random() * 10).toFixed(2));
+        const kabkotInflasi = kota.map(() => (Math.random() * 10).toFixed(2));
+
+        var rankBarChartProvinsi = new Chart(document.getElementById('rankBarChartProvinsi').getContext('2d'), {
+            type: 'bar',
+            options: {
+                indexAxis: 'y',
+                elements: {
+                    bar: {
+                        borderWidth: 2,
+                    },
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+            },
+            data: {
+                labels: provinsi,
+                datasets: [
+                    {
+                        label: 'Inflasi',
+                        data: provinsiInflasi,
+                        backgroundColor: '#4bc0c0',
+                    },
+                ],
+            },
+        });
+
+        var rankBarChartKabkot = new Chart(document.getElementById('rankBarChartKabkot').getContext('2d'), {
+            type: 'bar',
+            options: {
+                indexAxis: 'y',
+                elements: {
+                    bar: {
+                        borderWidth: 2,
+                    },
+                },
+                responsive: true,
+                maintainAspectRatio: true,
+            },
+            data: {
+                labels: kota,
+                datasets: [
+                    {
+                        label: 'Inflasi',
+                        data: kabkotInflasi,
+                        backgroundColor: '#36a2eb',
+                    },
+                ],
+            },
+        });
 
         // Bar Chart
         new Chart(document.getElementById('barChart'), {
